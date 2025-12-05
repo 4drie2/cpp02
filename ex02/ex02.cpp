3256aec295978ac14fc20cfdd7ba1e7ba4c59084
Adrien 42 <adr.bidaux@gmail.com>
2025-12-05 04:05:30 +0100

    add normal version and /0 protection

---
+++ ex02/ex02.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex02.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/05 03:59:12 by abidaux           #+#    #+#             */
+/*   Updated: 2025/12/05 04:03:06 by abidaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../FixedPoint/FixedPoint.hpp"
+
+int main( void ) {
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max( a, b ) << std::endl;
+
+	std::cout << "Division by zero: " << a/0 << std::endl;
+
+	return 0;
+}
